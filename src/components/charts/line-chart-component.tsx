b9ffe4b24c7bfb442cf9d4ec3f4850084f115693
Narendran Seenivasan <narendranseenivasan@gmail.com>
2024-12-15 12:20:55 +0530

    line chart fix

--- src/components/charts/line-chart-component.tsx
+++ src/components/charts/line-chart-component.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { TrendingUp } from "lucide-react"
-import { CartesianGrid, LabelList, Line, LineChart, ResponsiveContainer, XAxis } from "recharts"
+import { AreaChart, CartesianGrid, LabelList, Line, LineChart, ResponsiveContainer, XAxis } from "recharts"
 
 import {
 	Card,
@@ -24,6 +24,12 @@ const chartData = [
 	{ month: "April", desktop: 73, mobile: 190 },
 	{ month: "May", desktop: 209, mobile: 130 },
 	{ month: "June", desktop: 214, mobile: 140 },
+	{ month: "July", desktop: 214, mobile: 140 },
+	{ month: "August", desktop: 214, mobile: 140 },
+	{ month: "September", desktop: 214, mobile: 140 },
+	{ month: "October", desktop: 214, mobile: 140 },
+	{ month: "November", desktop: 214, mobile: 140 },
+	{ month: "December", desktop: 214, mobile: 140 },
 ]
 
 const chartConfig = {
@@ -40,75 +46,76 @@ const chartConfig = {
 export function LineChartComponent() {
 	return (
 
-		<Card className="md:col-span-3 w-full max-w-3xl max-h-[400px]">
-			<CardHeader>
-				<CardTitle>Line Chart - Label</CardTitle>
-			</CardHeader>
-			<CardContent>
-				<ChartContainer
-					config={{
-						desktop: {
-							label: "Desktop",
-							color: "hsl(var(--chart-1))",
-						},
-						mobile: {
-							label: "Mobile",
-							color: "hsl(var(--chart-2))",
-						},
-					}}
-					className="h-[200px] sm:h-[250px]"
-				>
-					<ResponsiveContainer width="100%" height="100%">
-						<LineChart
-							data={chartData}
-							margin={{
-								top: 20,
-								right: 10,
-								left: 10,
-								bottom: 0,
-							}}
-						>
-							<CartesianGrid vertical={false} />
-							<XAxis
-								dataKey="month"
-								tickLine={false}
-								axisLine={false}
-								tickMargin={8}
-								tickFormatter={(value) => value.slice(0, 3)}
-							/>
-							<ChartTooltip
-								cursor={false}
-								content={<ChartTooltipContent indicator="line" />}
-							/>
-							<Line
-								dataKey="desktop"
-								type="natural"
-								stroke="var(--color-desktop)"
-								strokeWidth={2}
-								dot={{
-									fill: "var(--color-desktop)",
-								}}
-								activeDot={{
-									r: 6,
-								}}
-							>
-								<LabelList
-									position="top"
-									offset={12}
-									className="fill-foreground"
-									fontSize={12}
-								/>
-							</Line>
-						</LineChart>
-					</ResponsiveContainer>
-				</ChartContainer>
-			</CardContent>
-			<CardFooter className="flex-col items-start gap-2 text-sm">
-				<div className="flex gap-2 font-medium leading-none text-muted-foreground">
+		<Card className="md:col-span-3 max-h-[400px]">
+          <CardHeader>
+            <CardTitle>Line Chart - Label</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ChartContainer
+              config={{
+                desktop: {
+                  label: "Desktop",
+                  color: "hsl(var(--chart-1))",
+                },
+                mobile: {
+                  label: "Mobile",
+                  color: "hsl(var(--chart-2))",
+                },
+              }}
+              className="w-full h-[200px] sm:h-[250px]"
+            >
+              <ResponsiveContainer width="100%" height="100%">
+                <AreaChart
+                  data={chartData}
+                  margin={{
+                    top: 20,
+					right: 10,
+					left: 10,
+					bottom: 0,
+                  }}
+                >
+                  <CartesianGrid vertical={false} />
+                  <XAxis
+                    dataKey="month"
+                    tickLine={false}
+                    axisLine={false}
+                    tickMargin={8}
+                    tick={{ fontSize: 12 }}
+                    interval={0}
+                  />
+                  <ChartTooltip
+                    cursor={false}
+                    content={<ChartTooltipContent indicator="line" />}
+                  />
+                  <Line
+                    dataKey="desktop"
+                    type="natural"
+                    stroke="var(--color-desktop)"
+                    strokeWidth={2}
+                    dot={{
+                      fill: "var(--color-desktop)",
+                    }}
+                    activeDot={{
+                      r: 6,
+                    }}
+                  >
+                    <LabelList
+                      position="top"
+                      offset={8}
+                      className="fill-foreground"
+                      fontSize={10}
+                    />
+                  </Line>
+                </AreaChart>
+              </ResponsiveContainer>
+            </ChartContainer>
+          </CardContent>
+          <CardFooter className="flex-col items-start gap-2 text-sm">
+		  		<div className="flex gap-2 font-medium leading-none text-muted-foreground">
 					Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
 				</div>
-			</CardFooter>
-		</Card>
+          </CardFooter>
+        </Card>
 
 	)
 }
